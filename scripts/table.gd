fbc78ac8aee5245baec796957992f6ab33d36460
Chris <seadonchristopher@gmail.com>
2025-03-30 22:23:19 +0100

    remove useless line of code

--- scripts/table.gd
+++ scripts/table.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 
 func _on_slot_0_child_entered_tree(node: Node) -> void:
-	print(node)
-	node.position = Vector2i(0,0) #Move node to 0 (relative to this slot)
+	pass
 
 
 func _on_slot_1_child_entered_tree(node: Node) -> void:
