6baf52a2795ce1584f4837b311a3ae8e9f6289e0
Christopher Seadon <cts43@bath.ac.uk>
2025-04-11 23:39:36 +0100

    Add time to next beat function

--- scripts/fmod_event_emitter_2d.gd
+++ scripts/fmod_event_emitter_2d.gd
@@ -1,11 +1,47 @@
 extends FmodEventEmitter2D
 
+const NEXT_BEAT_TOLERANCE = 1
+
+var startTime : float
+var tempo : float
+var fmodPosition : float
+var lastBeatTime : float
+var beatInterval = null
+var timeIntoCurrentBeat : float
+
 func _ready() -> void:
 	play()
+	
 
-func _on_timeline_beat(params: Dictionary) -> void:
-	print(params)
 # this signal sends these params which contains the current beat, bar, tempo etc. pretty helpful
 # we could/should probably be using this in some other script but this is good for an example
 
 #timeline marks in the fmod studio also send a signal, so we can use those on tempo changes etc. if necessary
+func _on_timeline_beat(params: Dictionary) -> void:
+	fmodPosition = params["position"]
+	lastBeatTime = Time.get_ticks_msec()
+	tempo = params["tempo"]
+	if not beatInterval:
+		beatInterval = 60000.0 / tempo
+	
+	#for testing: seems to be within 3ms of accuracy which should be good enough given how big our hit windows will be
+	#print(beatInterval)
+	#
+	#await get_tree().create_timer(0.3).timeout
+	#
+	#print("time to next: ",timeToNextBeat())
+	
+func timeToNextBeat():
+	#calculate based on godot time and last known fmod time+position for max accuracy.
+	var currentTime = Time.get_ticks_msec()
+	var elapsedTime = currentTime - lastBeatTime
+	var currentPosition = fmodPosition + elapsedTime
+	
+	var timeIntoCurrentBeat = fmod(currentPosition,beatInterval)
+	var timeToNextBeat = beatInterval - timeIntoCurrentBeat
+	
+	if timeToNextBeat < NEXT_BEAT_TOLERANCE: #if calling this function too close to the start of a beat, it gets confused.
+		#if we're pretty much on the beat the time to next can just be the constant time between beats based on tempo.
+		timeToNextBeat = beatInterval
+	
+	return timeToNextBeat
