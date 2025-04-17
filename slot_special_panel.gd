84e73a6e5c864503f4c59b0c1075e0c58a3ed9c0
Chris <seadonchristopher@gmail.com>
2025-04-17 01:25:29 +0100

    Add cool bloingo warble border. Mess with scaling yet again

---
+++ slot_special_panel.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+var i = 0
+var warbling = false
+var warble_amount = 0.009
+@export var SpecialSlot : Node2D
+@export var PlayerSignals : Node2D
+
+func _physics_process(delta: float) -> void:
+	if warbling:
+		i += 1
+		material.set_shader_parameter("time", Time.get_ticks_msec() / 1000.0)
+		if i >= 100:
+			i = 0
+
+func _on_slot_special_child_entered_tree(node: Node) -> void:
+	await PlayerSignals.card_animation_finished
+	material.set_shader_parameter("strength", warble_amount)
+	warbling = true
