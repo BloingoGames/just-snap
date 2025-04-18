bfe7c46dbd5e2e561c07c9456223c3eb8a1e096e
Chris <seadonchristopher@gmail.com>
2025-04-18 22:35:09 +0100

    Add new bloingo cards and placeholder/funny music

--- data/Player0Deck.gd
+++ data/Player0Deck.gd
@@ -1,4 +1,5 @@
 var deck = {
 	#key is the card being replaced. this way a special card could e.g replace a 2 with a king or something, but we still only cards 1:!
-	"5Spades" : {"Suit" : "Spades", "Pip" : "5", "Name" : "special_card-5C_sun","playableBeats" : [true,false,true,false,true,false,true], "bloingoEffect" : Global.BloingoEffect2}
+	"5Spades" : {"Suit" : "Spades", "Pip" : "5", "Name" : "special_card-5S_sun","playableBeats" : [true,false,true,false,true,false,true], "bloingoEffect" : Global.BloingoEffect2},
+	"6Diamonds" : {"Suit" : "Diamonds", "Pip" : "6", "Name" : "special_card-6D_worn","playableBeats" : [true,true,false,false,true,false,true], "bloingoEffect" : Global.BloingoEffect2}
 }
--- data/Player1Deck.gd
+++ data/Player1Deck.gd
@@ -1,3 +1,4 @@
 var deck = {
-	"9Spades" : {"Suit" : "Spades", "Pip" : "9", "Name" : "special_card-9C_sea","playableBeats" : [true,false,true,false,true,false,true], "bloingoEffect" : Global.BloingoEffect2}
+	"9Spades" : {"Suit" : "Spades", "Pip" : "9", "Name" : "special_card-9S_sea","playableBeats" : [true,false,true,false,true,false,true], "bloingoEffect" : Global.BloingoEffect2},
+	"9Hearts" : {"Suit" : "Hearts", "Pip" : "9", "Name" : "special_card-9H_crypt","playableBeats" : [true,false,true,false,true,false,true], "bloingoEffect" : Global.BloingoEffect2},
 }
